b8f4b157a1f8b2127591ccd07c0fecea75b6c925
Diogo Melo <diogro@gmail.com>
2014-09-15 17:41:24 -0300

    fluf

--- static/pdfs/roteiros_listas/2014-pratica1-hist.R
+++ static/pdfs/roteiros_listas/2014-pratica1-hist.R
@@ -8,7 +8,7 @@ PlotHistograms = function(file_url, cor){
     ggplot(m_cenario, aes(value, group = variable)) + geom_histogram(fill=cor, binwidth = 0.5)+ facet_wrap(~variable) + theme_bw()+ xlab("Numero de Pretos")+ ylab("Contagem")+ labs(title=names(cenario_df)[1])
 }
 
-PlotTimeSeries = function(file_url, cor){
+PlotTimeSeries = function(file_url){
     cenario_df = read.csv(textConnection(file_url))
     m_cenario = melt(cenario_df[,-1], id.vars = "Grupo")
     m_cenario$variable = as.numeric(gsub('geracao.', '', m_cenario$variable))
@@ -22,21 +22,19 @@ PlotTimeSeries = function(file_url, cor){
 
 c1=getURL("https://docs.google.com/spreadsheets/d/1jZbxJzstOdgaNADw6n1ov4pKo0l0dAD1uEcefAlFplI/export?gid=0&format=csv", ssl.verifypeer=FALSE)
 PlotHistograms(c1, 'lightskyblue2')
-PlotTimeSeries(c1, 'lightskyblue2')
+PlotTimeSeries(c1)
 
 c2=getURL("https://docs.google.com/spreadsheets/d/1jZbxJzstOdgaNADw6n1ov4pKo0l0dAD1uEcefAlFplI/export?gid=1488503095&format=csv", ssl.verifypeer=FALSE)
 PlotHistograms(c2, 'lightgoldenrod1')
-PlotTimeSeries(c2, 'lightgoldenrod1')
+PlotTimeSeries(c2)
 
 c3=getURL("https://docs.google.com/spreadsheets/d/1jZbxJzstOdgaNADw6n1ov4pKo0l0dAD1uEcefAlFplI/export?gid=1830499974&format=csv", ssl.verifypeer=FALSE)
 PlotHistograms(c3, 'lightpink3')
-PlotTimeSeries(c3, 'lightpink3')
+PlotTimeSeries(c3)
 
 c4=getURL("https://docs.google.com/spreadsheets/d/1jZbxJzstOdgaNADw6n1ov4pKo0l0dAD1uEcefAlFplI/export?gid=1573305252&format=csv", ssl.verifypeer=FALSE)
 PlotHistograms(c4, 'palegreen3')
-PlotTimeSeries(c4, 'palegreen3')
-
-ggplot(filter(m_cenario, Grupo ==  '23'), aes(variable, value, group = Grupo)) + geom_line()
+PlotTimeSeries(c4)
 
 ###################
 ####  Noturno  ####
