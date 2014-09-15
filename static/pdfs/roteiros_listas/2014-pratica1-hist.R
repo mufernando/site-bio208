97873b52703394f30ac1c59d678a011b1ef3c915
Diogo Melo <diogro@gmail.com>
2014-09-15 14:27:40 -0300

    nomes descentes

--- static/pdfs/roteiros_listas/2014-pratica1-hist.R
+++ static/pdfs/roteiros_listas/2014-pratica1-hist.R
@@ -5,7 +5,7 @@ if(!require(reshape2)){install.packages('reshape2'); library(reshape2)}
 PlotHistograms = function(file_url, cor){
     cenario_df = read.csv(textConnection(file_url))
     m_cenario = melt(cenario_df[,-1], id.vars = "Grupo")
-    ggplot(m_cenario, aes(value, group = variable)) + geom_histogram(fill=cor, binwidth = 0.5)+ facet_wrap(~variable) + theme_bw()+ xlab("Numero de Azuis")+ ylab("Frequencia")+ labs(title=names(cenario_df)[1])
+    ggplot(m_cenario, aes(value, group = variable)) + geom_histogram(fill=cor, binwidth = 0.5)+ facet_wrap(~variable) + theme_bw()+ xlab("Numero de Pretos")+ ylab("Contagem")+ labs(title=names(cenario_df)[1])
 }
 
 ##################
@@ -13,6 +13,7 @@ PlotHistograms = function(file_url, cor){
 ##################
 
 c1=getURL("https://docs.google.com/spreadsheets/d/1jZbxJzstOdgaNADw6n1ov4pKo0l0dAD1uEcefAlFplI/export?gid=0&format=csv", ssl.verifypeer=FALSE)
+file_url = c1
 c2=getURL("https://docs.google.com/spreadsheets/d/1jZbxJzstOdgaNADw6n1ov4pKo0l0dAD1uEcefAlFplI/export?gid=1488503095&format=csv", ssl.verifypeer=FALSE)
 c3=getURL("https://docs.google.com/spreadsheets/d/1jZbxJzstOdgaNADw6n1ov4pKo0l0dAD1uEcefAlFplI/export?gid=1830499974&format=csv", ssl.verifypeer=FALSE)
 c4=getURL("https://docs.google.com/spreadsheets/d/1jZbxJzstOdgaNADw6n1ov4pKo0l0dAD1uEcefAlFplI/export?gid=1573305252&format=csv", ssl.verifypeer=FALSE)
